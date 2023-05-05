e7a975314583ba0c85a0e9a6b0244005637a2431
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-05-05 20:46:25 +0300

    fixed

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -19,7 +19,7 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   );
 };
 
-// FeedbackOptions.propTypes = {
-//   options: PropTypes.arrayOf(PropTypes.string),
-//   onLeaveFeedback: PropTypes.func,
-// };
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string),
+  onLeaveFeedback: PropTypes.func,
+};
